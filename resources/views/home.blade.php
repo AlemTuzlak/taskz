fbec1c5487725c5da5fc3e4383b919886b16a044
Amna Sinanović <amna995@outlook.com>
2019-05-21 02:54:12 +0200

    Add files via upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,33 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+        <div class="col-md-12">
+            <div class="card primary-border">
+                <div class="card-header text-center text-white bg-danger primary-border">
+                    <h4>
+                        Boards
+                    </h4>
+                </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                @if(!count($user->boards))
+                <div class="card-body text-center">
+                    You have no boards yet, create a new one!
+                </div>
+                @else
+                <div class="row mg0">
+                    @foreach($user->boards as $board)
+                        @include('boards.card', compact($board))
+                    @endforeach
+                </div>
 
-                    You are logged in!
+                @endif
+                <div class="card-footer">
+                    <button id="board-button" data-toggle="modal" data-target="#board-modal" class="float-right btn btn-info">Create a board</button>
                 </div>
             </div>
         </div>
     </div>
 </div>
+    @include('modals.edit_board')
+    @include('modals.create_board')
 @endsection
